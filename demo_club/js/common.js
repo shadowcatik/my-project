4262b62520219731cb05491bb6122adce1f40824
Oksana Melnik <kyoshiniimura@gmail.com>
2016-09-29 16:32:26 +0300

    demo_club fix mob

--- demo_club/js/common.js
+++ demo_club/js/common.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
 
-   
+  //Slider
+  
   var slideCount = $('#slider ul li').length;
   var slideWidth = $('#slider ul li').width();
   var slideHeight = $('#slider ul li').height();
@@ -37,5 +38,46 @@ jQuery(document).ready(function ($) {
     $('.slider__controls-button--next').click(function () {
         moveRight();
     });
+    
+  //show gallery
+
+  var largeImg = document.getElementById('largeImg');
+
+    var thumbs = document.getElementById('thumbs');
+
+    thumbs.onclick = function(e) {
+      var target = e.target;
+
+      while (target != this) {
+
+        if (target.nodeName == 'A') {
+          $('.show-img').fadeIn();
+          showThumbnail(target.href, target.title);
+          return false;
+        }
+
+        target = target.parentNode;
+      }
+    }
+
+    function showThumbnail(href, title) {
+      largeImg.src = href;
+      largeImg.alt = title;
+    }
+
+    //предзагрузка
+    var imgs = thumbs.getElementsByTagName('img');
+    for (var i = 0; i < imgs.length; i++) {
+      var url = imgs[i].parentNode.href;
+      var img = document.createElement('img');
+      img.src = url;
+    }
+    //Закрытие  большого изображения
+
+    $('.close').click(function() {
+      $('.show-img').fadeOut();
+      return false;
+    });
+
 
 });    
